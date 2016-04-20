ca0599c5ea6e5b91ca11771da82624d27dbadae6
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-20 09:10:03 -0600

    created navbar and navbar controller

---
+++ app/assets/javascripts/app/Controllers/NavbarController.js
@@ -0,0 +1,29 @@
+angular
+.module('app')
+.controller('NavbarController', NavbarController);
+
+function NavbarController($scope, Auth, $state) {
+
+  $scope.signedIn = Auth.isAuthenticated;
+
+  $scope.logout = Auth.logout;
+
+  Auth.currentUser()
+    .then(function(user) {
+      $scope.user = user;
+    });
+
+  $scope.$on('devise:new-registration', function(e, user) {
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:login', function(e, user) {
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:logout', function(e, user) {
+    $scope.user = {};
+    
+  });
+
+};
