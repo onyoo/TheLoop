6f0f9cd602777daa06a29164578381f32d19373d
frankNowinski <nowinski.frank@gmail.com>
2016-04-21 14:16:17 -0400

    add validations to login/register - add css styling

--- app/assets/javascripts/app/Controllers/AuthController.js
+++ app/assets/javascripts/app/Controllers/AuthController.js
@@ -3,12 +3,19 @@ function AuthController($scope, $state, $location, Auth){
   $scope.login = function() {
     Auth.login($scope.user).then(function(){
       $state.go('home.events');
+    }, function(error){
+      $('.auth-errors').html(error.data.error);
     });
   };
 
   $scope.register = function() {
     Auth.register($scope.user).then(function() {
       $state.go('home.events');
+    }, function(error){
+      $('.auth-errors').html('Email ' + error.data.errors.email);
+      if (error.data.errors.password != undefined) {
+        $('.auth-errors').html('Password ' + error.data.errors.password);
+      }
     });
   };
 }
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   respond_to :json
-  
+
   after_filter :set_csrf_cookie_for_ng
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def home
   end
 
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+  
   def set_csrf_cookie_for_ng
     # form_authenticity_token sets a random string for the XSRF-TOKEN if protection is needed
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
