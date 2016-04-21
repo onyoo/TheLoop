664afcd1b9a5fc9a99faae4c617bfdb7b23e8d24
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-21 13:18:40 -0600

    user factory is working. Need to create seperate components for api events vs local events

--- app/assets/javascripts/app/Controllers/UserEventsController.js
+++ app/assets/javascripts/app/Controllers/UserEventsController.js
@@ -1,9 +1,10 @@
-function UserEventsController(UserEvent, Auth) {
+function UserEventsController(User, Auth) {
 
   var ctrl = this;
   // change our ctrl.notes = []; to query the entire Note table
   ctrl.user = Auth.currentUser().then(function(user) {
-    ctrl.events = UserEvent.query({ 'user_id': user.id });
+    ctrl.events = User.get({ 'id': user.id });
+    
   });
 
 };
