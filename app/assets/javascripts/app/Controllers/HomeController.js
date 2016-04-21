c79afd4dde1e2a01e3b5943322f758b8f48d3993
onyoo <roberto9m9@gmail.com>
2016-04-21 12:16:57 +0200

    adds My Events to navbar, touchup to local_view format

--- app/assets/javascripts/app/Controllers/HomeController.js
+++ app/assets/javascripts/app/Controllers/HomeController.js
@@ -9,23 +9,28 @@ function LocalController($scope, uiGmapIsReady) {
       control : {}
   };
 
-navigator.geolocation.getCurrentPosition(function(position) {
-
-  $scope.map = {
-      center : {
+  if(!!navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+                      // can use .watchPosition(); for periodic updates to coords
+      $scope.map = {
+        center : {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
-      },
-      zoom : 18,
-      control : {}
-  };
+        },
+        zoom : 18,
+        control : {}
+      };
 
-  uiGmapIsReady.promise()
-  .then(function (map_instances) {
-    var map1 = $scope.map.control;    // get map object through $scope.map.control getGMap() function
-    var map2 = map_instances[0].map;            // get map object through array object returned by uiGmapIsReady promise
-  });
-});
+      uiGmapIsReady.promise()
+      .then(function (map_instances) {
+        var map1 = $scope.map.control;    // get map object through $scope.map.control getGMap() function
+        var map2 = map_instances[0].map;            // get map object through array object returned by uiGmapIsReady promise
+      });
+    });
+  } else {
+      console.log('Sorry, your browser does not support geolocation.')
+      document.getElementById('your_location_map').innerHTML = 'Sorry, Your Browser Does Not Support Geolocation.';
+  }
 
 
 
