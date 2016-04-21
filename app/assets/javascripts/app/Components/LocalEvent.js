2f1a3015ac6b7426875cbefe9a5e78306c157663
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-21 15:11:34 -0600

    api component is working now to fix the local component

---
+++ app/assets/javascripts/app/Components/LocalEvent.js
@@ -0,0 +1,11 @@
+var LocalEvent = {
+  templateUrl: 'events/local_event.html',
+  bindings: {
+    details: '='
+  },
+  controllerAs: 'event'
+};
+
+angular
+  .module('app')
+  .component('localEvent', LocalEvent);
