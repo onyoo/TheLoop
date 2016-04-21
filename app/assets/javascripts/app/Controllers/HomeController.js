20bbe87fdb20e7a859e5c9d9a613a3bd6d21f75b
onyoo <roberto9m9@gmail.com>
2016-04-21 11:50:34 +0200

    closes #22 uses html5 geolocation feature to get your coords

--- app/assets/javascripts/app/Controllers/HomeController.js
+++ app/assets/javascripts/app/Controllers/HomeController.js
@@ -9,13 +9,25 @@ function LocalController($scope, uiGmapIsReady) {
       control : {}
   };
 
+navigator.geolocation.getCurrentPosition(function(position) {
+
+  $scope.map = {
+      center : {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+      },
+      zoom : 18,
+      control : {}
+  };
 
   uiGmapIsReady.promise()
   .then(function (map_instances) {
-
     var map1 = $scope.map.control;    // get map object through $scope.map.control getGMap() function
     var map2 = map_instances[0].map;            // get map object through array object returned by uiGmapIsReady promise
   });
+});
+
+
 
 };
 
