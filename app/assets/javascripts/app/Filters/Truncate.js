e66eb651f43a19047c3c33a18646b00119af78d3
frankNowinski <nowinski.frank@gmail.com>
2016-04-22 09:22:27 -0400

    fix truncate filter error

--- app/assets/javascripts/app/Filters/Truncate.js
+++ app/assets/javascripts/app/Filters/Truncate.js
@@ -1,13 +1,15 @@
 function Truncate () {
   return function (text, length, end) {
-    var maxLength = 250;
-    var ellipsis = "...";
+    if (text != undefined) { 
+      var maxLength = 250;
+      var ellipsis = "...";
 
-    if (text.length <= maxLength || text.length - ellipsis.length <= maxLength) {
-      return text;
-    }
-    else {
-      return String(text).substring(0, maxLength - ellipsis.length) + ellipsis;
+      if (text.length <= maxLength || text.length - ellipsis.length <= maxLength) {
+        return text;
+      }
+      else {
+        return String(text).substring(0, maxLength - ellipsis.length) + ellipsis;
+      }
     }
   }
 }
