67ae16aceb47ccb0b938a664fb6168723d6073ef
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-22 12:09:44 -0600

    pushing up changes so roberto can have no errors

---
+++ app/assets/javascripts/app/Components/LoopIndexEvent.js
@@ -0,0 +1,15 @@
+var LoopIndexEvent = {
+  templateUrl: 'events/loop_index_event.html',
+  bindings: {
+    details: '='
+  },
+  controller: function(UserEvent){
+    // UserEvent.query({id: this.details.id});
+    this.date = Date.parse(this.details.start_time);
+  },
+  controllerAs: 'event'
+};
+
+angular
+  .module('app')
+  .component('apiEvent', ApiEvent);
