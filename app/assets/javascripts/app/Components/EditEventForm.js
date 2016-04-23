acb8be712b3c586ee1861f73d35271961a57e2b0
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-23 14:56:52 -0600

    created edit event form component

---
+++ app/assets/javascripts/app/Components/EditEventForm.js
@@ -0,0 +1,28 @@
+var EditEventForm = {
+  templateUrl: 'events/edit_event_form.html',
+  controller: function(UserEvent, $scope, $stateParams) {
+    var ctrl = this;
+
+    $scope.closeForm = function() {
+      $scope.$emit('closeForm', false);
+    }
+
+    $scope.selectedCountry = [];
+
+    ctrl.event = UserEvent.get({id: $stateParams.id});
+
+    ctrl.editEvent = function() {
+      ctrl.event.$update(function() {
+        $location.path('events')
+      });
+    };
+    ctrl.message = "this is the controller"
+  },
+  controllerAs: 'eventForm'
+};
+
+angular
+  .module('app')
+  .component('editEventForm', EditEventForm);
+
+  
