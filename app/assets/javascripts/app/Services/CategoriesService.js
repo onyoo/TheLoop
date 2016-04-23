cc2e042a62bf18f7e3a345d457367f64da780bd9
onyoo <roberto9m9@gmail.com>
2016-04-23 21:14:57 +0200

    adds categories index route and service call

---
+++ app/assets/javascripts/app/Services/CategoriesService.js
@@ -0,0 +1,14 @@
+function CategoriesService($http) {
+
+  this.getCategories = function() {
+    return $http({
+      method: 'get',
+      url: 'http://localhost:3000/api/v1/categories'
+    });
+  }
+  
+}
+
+angular
+  .module('app')
+  .service('categoriesService', CategoriesService);
