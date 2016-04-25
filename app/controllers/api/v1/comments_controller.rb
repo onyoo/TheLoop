db31234314848dacb88e61d66a57ff4f5563ceac
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-25 09:01:43 -0600

    created comments controller for rails api

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,44 @@
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      skip_before_filter :verify_authenticity_token
+
+      before_filter :authenticate_user!, only: [:create, :destroy]
+
+      respond_to :json
+
+      def index
+        @event = Event.find(params[:event_id)
+        @comments = event.comments
+        respond_with @event, @comments
+      end
+
+      def show
+        @event = Event.find(params[:event_id])
+        @comment = @event.comments.find(params[:id])
+        respond_with @event, @comments
+      end
+
+      def create
+        @event = Event.find(params[:hike_id])
+        @comment = @event.comments.create(comment_params)
+        respond_with @event, @comment
+      end
+
+      
+
+      def destroy
+        @event = Event.find(params[:hike_id])
+        @comment = @event.comments.find(params[:id])
+        respond_with @comment.destroy
+      end
+
+      private
+        def comment_params
+          params.require(:comment).permit(:content, :event_id, :user_id)
+        end
+
+
+    end
+  end
+end
