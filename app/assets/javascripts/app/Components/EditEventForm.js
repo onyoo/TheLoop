00cd02eada8e40f159bc105fa9dbf3fd7cedddb8
onyoo <roberto9m9@gmail.com>
2016-04-26 21:23:58 +0200

    adds better category selection

--- app/assets/javascripts/app/Components/EditEventForm.js
+++ app/assets/javascripts/app/Components/EditEventForm.js
@@ -13,11 +13,12 @@ var EditEventForm = {
     ctrl.event.data.start_time = new Date(ctrl.event.data.start_time);
     $scope.selectedCountry = [];
 
-    ctrl.allCategories = '';
+    ctrl.allCategories = [];
 
     ctrl.categories = CategoriesService.getCategories().then(function(res){
       ctrl.allCategories = res.data.map(function(category){
-        return category.name.replace('&amp; ', '');
+        category.name = category.name.replace('&amp; ', '');
+        return category;
       });
     });
 
